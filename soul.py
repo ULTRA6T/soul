54ed750e1b99483c44715885fd3d8d71e81c7874
ULTRA6T <gcrohi@gmail.com>
2024-09-25 12:50:14 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7532967183'
+TOKEN = '7420207876:AAHEzpg8Uhb9ObUYU_jyZOHjRlcIjVsolJo'
 MONGO_URI = 'mongodb+srv://Soul:JYAuvlizhw7wqLOb@soul.tsga4.mongodb.net'
-FORWARD_CHANNEL_ID = -100
-CHANNEL_ID = -100
-error_channel_id = -100
+FORWARD_CHANNEL_ID = -1002361297772
+CHANNEL_ID = -1002361297772
+error_channel_id = -1002361297772
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
